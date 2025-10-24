47eba510325d5e660ae126e90ca8109c004b805e
marcostech99 <vinmarvtech@gmail.com>
2025-10-24 09:56:53 -0300

    chore: jest setup

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^.+\.module\.(css|sass|scss)$': 'identity-obj-proxy',
+
+    '^.+\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+    '^.+\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.next/',
+  ],
+}
+
+export default createJestConfig(config)
